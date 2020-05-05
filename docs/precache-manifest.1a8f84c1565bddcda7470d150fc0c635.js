40c380577921b927bcdb9a3b5ab4ed71fa496b19
Pavel Khrustalyov <pavel_khrustalyov@mail.ru>
2020-05-05 21:07:25 +0300

    first commit

---
+++ docs/precache-manifest.1a8f84c1565bddcda7470d150fc0c635.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c948f18603c999b1c8a4f2f52a121065",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4067b6f1526440235d0d",
+    "url": "/static/css/main.e1f5a8df.chunk.css"
+  },
+  {
+    "revision": "647e8ee4fc18c090f8c4",
+    "url": "/static/js/2.37af6ec2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.37af6ec2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4067b6f1526440235d0d",
+    "url": "/static/js/main.da3f0131.chunk.js"
+  },
+  {
+    "revision": "05f22b442774d5aee8a6",
+    "url": "/static/js/runtime-main.2934c6cf.js"
+  },
+  {
+    "revision": "0639b92c8c8a9bee98c3b8c644ddd688",
+    "url": "/static/media/dylan.0639b92c.mp3"
+  },
+  {
+    "revision": "540b9f3e55c4c5ae6201da60668ba970",
+    "url": "/static/media/miron1.540b9f3e.mp3"
+  },
+  {
+    "revision": "bb2a58b13e1bc52e79cd27044ee22977",
+    "url": "/static/media/miron2.bb2a58b1.mp3"
+  },
+  {
+    "revision": "dfd5438183b551822ed7bc4dc92496e4",
+    "url": "/static/media/scorpions.dfd54381.mp3"
+  },
+  {
+    "revision": "1c16762f1c1d60c88c1049834360db85",
+    "url": "/static/media/sting.1c16762f.mp3"
+  },
+  {
+    "revision": "267774a0799940209360cb53e01f6b08",
+    "url": "/static/media/usher.267774a0.mp3"
+  }
+]);
